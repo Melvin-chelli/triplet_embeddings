8d0269a9193295e60dec46c42fb4443f66948e5d
Melvin Chelli <melvin.chelli@dfki.de>
2023-07-19 08:29:57 +0000

    Combined triplet generator, compilation ok, functionality, NOT

--- genereateTriplets.py
+++ genereateTriplets.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from torch.utils.data import Dataset
 import numpy as np
-
+import matplotlib.pyplot as plt
 class TripletMNIST(Dataset):
     """
     Train: For each sample (anchor) randomly chooses a positive and negative samples
@@ -12,7 +12,6 @@ class TripletMNIST(Dataset):
         self.mnist_dataset = mnist_dataset
         self.train = self.mnist_dataset.train
         self.transform = self.mnist_dataset.transform
-
         if self.train:
             self.train_labels = self.mnist_dataset.train_labels
             self.train_data = self.mnist_dataset.train_data
@@ -64,6 +63,83 @@ class TripletMNIST(Dataset):
             img2 = self.transform(img2)
             img3 = self.transform(img3)
         return (img1, img2, img3), []
+
+    def __len__(self):
+        return len(self.mnist_dataset)
+    
+    
+class CombinedTriplet(Dataset):
+    """
+    Train: For each sample (anchor) randomly chooses a positive and negative samples
+    Test: Creates fixed triplets for testing
+    """
+
+    def __init__(self, mnist_dataset, train=False):
+        self.mnist_dataset = mnist_dataset
+        self.train = train
+        self.train_labels = []
+        self.train_data = []
+        self.test_labels = []
+        self.test_data = []
+        test_data = []
+        test_labels = []
+
+        if self.train:
+            for i in range(len(mnist_dataset)):
+                data,label = mnist_dataset.__getitem__(i)
+                self.train_labels.append(label)
+                self.train_data.append(data)
+            self.labels_set = set(self.train_labels)
+            self.label_to_indices = {label: np.where(np.asarray(self.train_labels) == label)[0]
+                                     for label in self.labels_set}
+
+        else:
+            for i in range(len(mnist_dataset)):
+                data,label = mnist_dataset.__getitem__(i)
+                self.test_labels.append(label)
+                self.test_data.append(data)
+            # generate fixed triplets for testing
+            self.labels_set = set(self.test_labels)
+            self.label_to_indices = {label: np.where(np.asarray(self.test_labels) == label)[0]
+                                     for label in self.labels_set}
+
+            random_state = np.random.RandomState(29)
+            triplets = [[i,
+                         random_state.choice(self.label_to_indices[self.test_labels[i]]),
+                         random_state.choice(self.label_to_indices[
+                                                 np.random.choice(
+                                                     list(self.labels_set - set([self.test_labels[i]])))])
+                         ]for i in range(len(self.test_data))]
+            self.test_triplets = triplets
+
+    def __getitem__(self, index):
+        if self.train:
+            img1, label1 = self.train_data[index], self.train_labels[index]
+            img1 = img1.numpy().reshape(28,28)
+
+            # print("DATA AND LABEL: "+ str(img1.size())+" ", str(label1)+" "+str(index))
+            positive_index = index
+            while positive_index == index:
+                positive_index = np.random.choice(self.label_to_indices[label1])
+            negative_label = np.random.choice(list(self.labels_set - set([label1])))
+            negative_index = np.random.choice(self.label_to_indices[negative_label])
+            img2 = self.train_data[positive_index]
+            img3 = self.train_data[negative_index]
+            img2 = img2.numpy().reshape(28,28)
+            img3 = img3.numpy().reshape(28,28)
+            # print(img2)
+        else:
+            img1 = self.test_data[self.test_triplets[index][0]].numpy().reshape(28,28)
+            img2 = self.test_data[self.test_triplets[index][1]].numpy().reshape(28,28)
+            img3 = self.test_data[self.test_triplets[index][2]].numpy().reshape(28,28)
+
+        img1 = Image.fromarray(img1, mode='L')
+        img2 = Image.fromarray(img2, mode='L')
+        img3 = Image.fromarray(img3, mode='L')
+
+        return (img1, img2, img3), []
+    
+
 
     def __len__(self):
         return len(self.mnist_dataset)
